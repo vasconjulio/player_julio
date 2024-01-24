22e04af12e2b6366ae0f95af43641f803f86bda6
Julio Vasconcelos <julioc43@gmail.com>
2024-01-24 15:25:06 -0300

    progress bar create

--- script.js
+++ script.js
@@ -5,82 +5,95 @@ const cover = document.getElementById('cover');
 const play = document.getElementById('play');
 const next = document.getElementById('next');
 const previous = document.getElementById('previous');
+const currentProgress = document.getElementById('current-progress');
+const progressContainer = document.getElementById('progress-container');
 
 const starboy = {
-    songName : 'Starboy',
-    artist : 'The Weeknd',
-    file : 'starboy',
+  songName: 'Starboy',
+  artist: 'The Weeknd',
+  file: 'starboy',
 };
 
 const saveYourTears = {
-    songName : 'Save Your Tears',
-    artist : 'The Weeknd',
-    file : 'save_your_tears',
+  songName: 'Save Your Tears',
+  artist: 'The Weeknd',
+  file: 'save_your_tears',
 };
 
 const hills = {
-    songName : 'The Hills',
-    artist : 'The Weeknd',
-    file : 'the_Hills',
+  songName: 'The Hills',
+  artist: 'The Weeknd',
+  file: 'the_Hills',
 };
 
 let isPlaying = false;
-const playlist = [starboy,saveYourTears,hills];
+const playlist = [starboy, saveYourTears, hills];
 let index = 0;
 
 function playSong(){
-    play.querySelector('.bi').classList.remove('bi-play-circle-fill');
-    play.querySelector('.bi').classList.add('bi-pause-circle-fill');
-    song.play();
-    isPlaying = true;
+  play.querySelector('.bi').classList.remove('bi-play-circle-fill');
+  play.querySelector('.bi').classList.add('bi-pause-circle-fill');
+  song.play();
+  isPlaying = true;
 }
-function pauseSong(){
-    play.querySelector('.bi').classList.add('bi-play-circle-fill');
-    play.querySelector('.bi').classList.remove('bi-pause-circle-fill');
-    song.pause();
-    isPlaying = false;
-}
-
-function playPauseDecider(){
-    if(isPlaying == true){
-        pauseSong();
-    }
-    else {
-        playSong();
-    }
+function pauseSong() {
+  play.querySelector('.bi').classList.add('bi-play-circle-fill');
+  play.querySelector('.bi').classList.remove('bi-pause-circle-fill');
+  song.pause();
+  isPlaying = false;
 }
 
-function initializeSong(){
-    cover.src = `images/${playlist[index].file}.jpg`;
-    song.src = `songs/${playlist[index].file}.mp3`;
-    songName.innerText = playlist[index].songName;
-    bandName.innerText = playlist[index].artist;
+function playPauseDecider() {
+  if (isPlaying == true) {
+    pauseSong();
+  } else {
+    playSong();
+  }
 }
 
-function previousSong(){
-    if(index === 0){
+function initializeSong() {
+  cover.src = `images/${playlist[index].file}.jpg`;
+  song.src = `songs/${playlist[index].file}.mp3`;
+  songName.innerText = playlist[index].songName;
+  bandName.innerText = playlist[index].artist;
+}
+
+function previousSong() {
+  if (index === 0) {
     index = playlist.length - 1;
-    }
-    else {
-        index -= 1;
-    }
-    initializeSong();
-    playSong();
+  } else {
+    index -= 1;
+  }
+  initializeSong();
+  playSong();
 }
 
-function nextSong(){
-    if(index === playlist.length - 1){
-        index - 0;
-    }
-    else {
-        index += 1;
-    }
-    initializeSong();
-    playSong();
+function nextSong() {
+  if (index === playlist.length - 1) {
+    index = 0;
+  } else {
+    index += 1;
+  }
+  initializeSong();
+  playSong();
+}
+
+function updateProgressBar(){
+    const barWidth = (song.currentTime/song.duration)*100;
+    currentProgress.style.setProperty('--progress', `${barWidth}%`)
+}
+
+function jumpTo(event){
+  const width = progressContainer.clientWidth;
+  const clickPosition = event.offsetX;
+  const jumpToTime = (clickPosition/width)* song.duration;
+  song.currentTime = jumpToTime;
 }
 
 initializeSong();
 
 play.addEventListener('click', playPauseDecider);
-previous.addEventListener('click',previousSong);
-next.addEventListener('click', nextSong)
+previous.addEventListener('click', previousSong);
+next.addEventListener('click', nextSong);
+song.addEventListener('timeupdate', updateProgressBar);
+progressContainer.addEventListener('click', jumpTo);
